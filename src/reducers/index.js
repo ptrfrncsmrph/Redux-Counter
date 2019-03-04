a582230b8c6617b2931f78d172ed20dfcf95a343
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-04 13:17:09 -0500

    Completes MVP

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT } from '../actions';
+import { INCREMENT, DECREMENT, RESET } from "../actions"
 
 const initialState = {
   count: 0
@@ -12,10 +12,12 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return { count: state.count + 1 }
     case DECREMENT:
-    // Fill in the body of this case
+      return { count: state.count - 1 }
+    case RESET:
+      return { count: 0 }
     default:
-      return state;
+      return state
   }
-};
+}
